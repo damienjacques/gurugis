06a5a5bd3447d8fb8b8e3fab5a615e78973dd351
damienjacques <damienjacques1989@gmail.com>
2020-01-23 12:23:01 +0100

    Add data and update readme

--- R/zonal_stats.R
+++ R/zonal_stats.R
@@ -33,28 +33,28 @@ zonal_pipe <- function(zone_in, raster_in, shp_out = NULL, stat) {
     # Load raster
     r <- stack(raster_in)
     # Load zone shapefile
-    shp <- readOGR(zone_in)
+    shp <- shapefile(zone_in)
     # Project 'zone' shapefile into the same coordinate system than the input raster
     shp <- spTransform(shp, crs(r))
-    
+
     # Add ID field to Shapefile
     shp@data$ID <- c(1:length(shp@data[, 1]))
-    
+
     # Crop raster to 'zone' shapefile extent
     r <- crop(r, extent(shp))
     # Rasterize shapefile
     zone <- rasterize(shp, r, field = "ID", dataType = "INT1U")  # Change dataType if nrow(shp) > 255 to INT2U or INT4U
-    
+
     # Zonal stats
     Zstat <- data.frame(myZonal(r, zone, stat))
     colnames(Zstat) <- c("ID", paste0(names(r), "_", c(1:(length(Zstat) - 1)), "_", stat))
-    
+
     # Merge data in the shapefile and write it
     shp@data <- plyr::join(shp@data, Zstat, by = "ID")
-    
+
     if (is.null(shp_out)) {
         return(shp)
     } else {
-        writeOGR(shp, shp_out, layer = sub("^([^.]*).*", "\\1", basename(zone_in)), driver = "ESRI Shapefile")
+        shapefile(shp, shp_out)
     }
 }
--- README.md
+++ README.md
@@ -1,17 +1,25 @@
 
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
 # Gurugis - the voice of GISdom <img src='man/figures/logo.png' align="right" height="139" />
 
 <!-- badges: start -->
+
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://www.tidyverse.org/lifecycle/#experimental)
 <!-- badges: end -->
 
-The goal of gurugis is to ...
+The goal of gurugis is to provide a series of functions useful to the
+regular GIS spatial analysis workflow.
 
 ## Installation
 
-You can install the released version of gurugis from [CRAN](https://CRAN.R-project.org) with:
+You can install the development version from
+[GitHub](https://github.com/) with:
 
 ``` r
-install.packages("gurugis")
+# install.packages("devtools")
+devtools::install_github("damienjacques/gurugis")
 ```
 
 ## Example
@@ -22,4 +30,3 @@ This is a basic example which shows you how to solve a common problem:
 library(gurugis)
 ## basic example code
 ```
-
